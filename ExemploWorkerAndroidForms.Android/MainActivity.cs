aa5f1538902d4e4624c4f8435822fe4363b3a0e1
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-01-27 15:52:02 -0300

    :tada: Subindo para o Github

---
+++ ExemploWorkerAndroidForms.Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using AndroidX.Work;
+
+namespace ExemploWorkerAndroidForms.Droid
+{
+    [Activity(Label = "ExemploWorkerAndroidForms", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+           
+            LoadApplication(new App());
+
+            //Processo do Worker Executa a cada 5 Segundos
+            var processo = PeriodicWorkRequest.Builder.From<MeuWorker>(TimeSpan.FromSeconds(5)).Build();
+            WorkManager.Instance.Enqueue(processo);
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ ExemploWorkerAndroidForms.Android/MeuWorker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Content;
+using Android.Util;
+using AndroidX.Work;
+using Xamarin.Essentials;
+
+namespace ExemploWorkerAndroidForms.Droid
+{
+    public class MeuWorker : Worker
+    {
+        public MeuWorker(Context context, WorkerParameters workerParams)
+          : base(context, workerParams)
+        {
+        }
+
+        public override Result DoWork()
+        {
+            try
+            {
+                Log.Info("Teste", $"Iniciando Serviço de BackGround - {DateTime.Now}");
+
+                Log.Info("Local", "Acessando para Testar");
+
+                return new Result.Retry();
+            }
+            catch(Exception ex)
+            {
+                return new Result.Failure();
+            }
+        }
+    }
+}
